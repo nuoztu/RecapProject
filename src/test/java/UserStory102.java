f42149fe6e7bea90ee6b7f14dc26e8a36b06a05c
nuoztu <nuoztu@gmail.com>
2021-06-30 20:19:30 -0500

    User story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,4 +1,7 @@
 public class UserStory102 {
 
-
+//lots of code here
+    //ready to push this branch to remote
+    //and open pull request so I can start collaborating
+    //so my team member can review and approve my code
 }
